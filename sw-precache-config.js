b86ae5457fb9d9e3c18a81a85aa3672f011b5a3c
Dhiviya Bharathi <dhiviarul@gmail.com>
2017-02-21 16:33:22 +0530

    Added new static files

---
+++ sw-precache-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    staticFileGlobs: [
+        '**.html',
+        'js/**.js',
+        'css/**.css',
+        'images/**'
+    ],
+    root: '/',
+    stripPrefix: '/',
+    navigateFallback: '/index.html'
+};
